bd498d5b50c8b8e0a55a26eb4b9c13dc47383756
Miranda W <miranda84315@gmail.com>
2018-11-14 21:14:33 +0800

    Fix combo box range value

---
+++ SaveVideo.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+
+'''
+This is use for save tracking result video
+And save in 
+video/camera1_result.avi
+video/camera2_result.avi
+...
+video/camera8_result.avi
+'''
